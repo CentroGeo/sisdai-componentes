d6022b27b47f3f73be7c83919e4fd52f392d8d99
ciretorres <erictorres.velasco@gmail.com>
2023-07-18 11:59:21 -0600

    agregando regla tabIndex para NavegacionPrincipal

--- src/componentes/navegacion-principal/SisdaiNavegacionPrincipal.vue
+++ src/componentes/navegacion-principal/SisdaiNavegacionPrincipal.vue
@@ -1,28 +1,3 @@
-<script setup>
-import { defineProps, ref } from 'vue'
-import { useMenuDesenfocable } from '../../composables/useMenuDesenfocable'
-
-defineProps({
-  navInformacion: {
-    default: '',
-    type: String,
-  },
-  fija: {
-    default: true,
-    type: Boolean,
-  },
-})
-
-//Que el menu se pueda cerrar automaticamente al enfocar otra cosa
-const cuadroElementosMenu = ref(null)
-const {
-  menuEstaAbierto,
-  alternarMenu,
-  // eslint-disable-next-line
-  alternarSubmenu,
-} = useMenuDesenfocable(cuadroElementosMenu)
-</script>
-
 <template>
   <nav
     class="navegacion navegacion-conacyt"
@@ -95,3 +70,72 @@ const {
     </div>
   </nav>
 </template>
+
+<script setup>
+import { defineProps, onMounted, ref, watch } from 'vue'
+import { useMenuDesenfocable } from '../../composables/useMenuDesenfocable'
+
+defineProps({
+  navInformacion: {
+    default: '',
+    type: String,
+  },
+  fija: {
+    default: true,
+    type: Boolean,
+  },
+})
+
+//Que el menu se pueda cerrar automaticamente al enfocar otra cosa
+const cuadroElementosMenu = ref(null)
+const {
+  menuEstaAbierto,
+  alternarMenu,
+  // eslint-disable-next-line
+  alternarSubmenu,
+} = useMenuDesenfocable(cuadroElementosMenu)
+
+const navMenu = document.getElementsByClassName('nav-menu')
+
+/**
+ * Agrega el atributo tabindex a los elementos de lista,
+ * si está en versión móvil
+ */
+function agregaAtributoTabIndex() {
+  if (window.innerWidth < 768) {
+    for (let index = 0; index < navMenu[1]['children'].length; index++) {
+      const element = navMenu[1]['children'][index]['children'][0]
+      element.tabIndex = '-1'
+    }
+  }
+}
+
+/**
+ * Si el menú está abierto en móvil, remueve el atributo tabIndex.
+ * Si está cerrado, agrega el atributo tabIndex en -1 para
+ * saltarse los enlaces con el teclado secuencial.
+ */
+function actualizaAtributoTabIndex(estaAbierto) {
+  if (window.innerWidth < 768) {
+    if (estaAbierto) {
+      for (let i = 0; i < navMenu[1]['children'].length; i++) {
+        const element = navMenu[1]['children'][i]['children'][0]
+        element.removeAttribute('tabIndex')
+      }
+    } else {
+      for (let j = 0; j < navMenu[1]['children'].length; j++) {
+        const element = navMenu[1]['children'][j]['children'][0]
+        element.tabIndex = '-1'
+      }
+    }
+  }
+}
+
+onMounted(() => {
+  agregaAtributoTabIndex()
+})
+
+watch(menuEstaAbierto, () => {
+  actualizaAtributoTabIndex(menuEstaAbierto.value)
+})
+</script>
