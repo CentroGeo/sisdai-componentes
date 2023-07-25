7ba98914da99b7049c15fa88ff5816e300458ac6
andres-geotec <andres.geotec@gmail.com>
2023-07-25 17:47:30 -0600

    auitando el uso de vue ruter en menú lateral

--- src/componentes/menu-accesibilidad/SisdaiMenuAccesibilidad.vue
+++ src/componentes/menu-accesibilidad/SisdaiMenuAccesibilidad.vue
@@ -160,9 +160,9 @@ const alturaMenuAbierto = computed(
         class="icono-accesibilidad icono-5"
         aria-hidden="true"
       />
-      <span class="a11y-solo-lectura"
-        >abrir y cerrar menú de accesibilidad</span
-      >
+      <span class="a11y-solo-lectura">
+        abrir y cerrar menú de accesibilidad
+      </span>
     </button>
 
     <menu class="menu-accesibilidad">
