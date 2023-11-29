3e73773d84055fa9d06df63e8b7ee7c4fe564e59
ciretorres <erictorres.velasco@gmail.com>
2023-11-28 20:09:51 -0600

    colocando el emit en los ejemplos de la documentación del menú lateral

--- docs/.vuepress/components/menu-lateral/basico.vue
+++ docs/.vuepress/components/menu-lateral/basico.vue
@@ -1,19 +1,48 @@
+<script setup>
+import { ref } from 'vue'
+const menuLateralAbierto = ref(false)
+function alAlternarMenuLateral(navSecundariaAbierta) {
+  menuLateralAbierto.value = navSecundariaAbierta
+}
+</script>
 <template>
   <div>
-    <SisdaiMenuLateral>
+    <SisdaiMenuLateral @alAlternarMenu="alAlternarMenuLateral">
       <template slot="contenido-menu-lateral">
         <ul>
           <li>
             <router-link
               to="#api"
               exact
+              :tabindex="menuLateralAbierto ? undefined : -1"
             >
               API</router-link
             >
           </li>
-          <li><router-link to="#slots"> Slots</router-link></li>
-          <li><router-link to="#ejemplos"> Ejemplos </router-link></li>
-          <li><a href="#menu-lateral"> El mero inicio </a></li>
+          <li>
+            <router-link
+              to="#slots"
+              :tabindex="menuLateralAbierto ? undefined : -1"
+            >
+              Slots</router-link
+            >
+          </li>
+          <li>
+            <router-link
+              to="#ejemplos"
+              :tabindex="menuLateralAbierto ? undefined : -1"
+            >
+              Ejemplos
+            </router-link>
+          </li>
+          <li>
+            <a
+              href="#menu-lateral"
+              :tabindex="menuLateralAbierto ? undefined : -1"
+            >
+              El mero inicio
+            </a>
+          </li>
         </ul>
       </template>
     </SisdaiMenuLateral>
