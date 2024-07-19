3eb2c742e1abbab9618d87e7877e8543514b160a
ciretorres <erictorres.velasco@gmail.com>
2024-07-18 18:43:13 -0600

    actualizando dependencias y agregando happy dom para el vitest config js

--- vitest.config.js
+++ vitest.config.js
@@ -6,7 +6,12 @@ export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
-      environment: 'jsdom',
+      // enable jest-like global test APIs
+      globals: true,
+      // simulate DOM with happy-dom
+      // (requires installing happy-dom as a peer dependency)
+      environment: 'happy-dom',
+      // environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
     },
