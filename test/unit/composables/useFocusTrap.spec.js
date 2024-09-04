e454d42ad02e849cc26b5e6024ade28d8f7713db
ciretorres <erictorres.velasco@gmail.com>
2024-09-04 15:09:41 -0600

    agregando pruebas unitarias para composables

--- test/unit/composables/useFocusTrap.spec.js
+++ test/unit/composables/useFocusTrap.spec.js
@@ -1,10 +1,39 @@
 import { expect, describe, it } from 'vitest'
-// import { useFocusTrap } from '../../../src/composables/useFocusTrap.js'
+import useFocusTrap from '../../../src/composables/useFocusTrap.js'
+import { mount } from '@vue/test-utils'
+
+import { createApp } from 'vue'
+
+function mockLoadComposableInApp(composable) {
+  let result
+  const app = createApp({
+    setup() {
+      result = composable()
+      // removiendo advertencia de template faltante
+      return () => {}
+    },
+  })
+  app.mount(document.createElement('div'))
+  return [result, app]
+}
+
+// const trapRef = ref(null)
+// const TestComponent = {
+//   template: `<dialog class="modal" ref="${trapRef.value}"><div class="modal-contenedor"><button type="button" class="boton-pictograma boton-sin-contenedor-secundario modal-cerrar" value="cerrar" @click=""><span class="pictograma-cerrar" aria-hidden="true"></span></button></div></dialog>`,
+// }
 
 describe('useFocusTrap', () => {
-  // const { trapRef } = useFocusTrap
   it('instancias de propiedades y métodos', () => {
-    // elementosEnfocables
-    // trapRef
+    // const wrapper = mount(TestComponent)
+    // console.log(wrapper.vm)
+
+    const result = mockLoadComposableInApp(() => useFocusTrap())
+
+    // console.log('result =', result[0])
+    expect(result[0]).toBeTypeOf('object')
+    result[0].iniciaFocusTrap()
+    result[0].terminaFocusTrap()
+    // console.log('trapRef =', result[0].trapRef.value)
+    expect(result[0].trapRef.value).toBe(null)
   })
 })
--- test/unit/composables/useMenuDesenfocable.spec.js
+++ test/unit/composables/useMenuDesenfocable.spec.js
@@ -1,13 +1,43 @@
 import { expect, describe, it } from 'vitest'
 import { useMenuDesenfocable } from '../../../src/composables/useMenuDesenfocable.js'
 
+import { createApp, ref } from 'vue'
+
+function mockLoadComposableInApp(composable) {
+  let result
+  const app = createApp({
+    setup() {
+      result = composable()
+      // removiendo advertencia de template faltante
+      return () => {}
+    },
+  })
+  app.mount(document.createElement('div'))
+  return [result, app]
+}
+
+const cuadroElementosMenuRef = ref(null)
+
+// const TestComponent = {
+//   template: `<div class="nav-menu-principal" ref="${cuadroElementosMenuRef.value}" tabindex="0"><slot><ul class="nav-menu"><li><a href="#" class="nav-hipervinculo" exact @mouseover="" @click="">Menú</a></li><li class="nav-contenedor-submenu"><button class="nav-boton-submenu" @mouseover="" @click="">Menú con submenu</button><ul class="nav-submenu"><li><button class="nav-boton-regresar" @click="">Submenu</button></li><li><a href="#eje" class="nav-hipervinculo" @click="">Submenu uno</a></li><li><a href="#aja" class="nav-hipervinculo" @click="">Submenu dos</a></li></ul></li></ul></slot></div>`,
+// }
+
 describe('useMenuDesenfocable', () => {
-  // TODO investigar lifecycle hooks
-  const { menuEstaAbierto } = useMenuDesenfocable
-  // console.log(useMenuDesenfocable().menuEstaAbierto)
   it('instancias de propiedades y métodos', () => {
-    // menuEstaAbierto
-    // submenuEstaAbierto
-    // esColapsable
+    const [result] = mockLoadComposableInApp(() =>
+      useMenuDesenfocable(cuadroElementosMenuRef)
+    )
+    // console.log(result)
+    expect(result.menuEstaAbierto.value).toBe(false)
+    result.abrirMenu()
+    expect(result.menuEstaAbierto.value).toBe(true)
+    result.cerrarMenu()
+    expect(result.menuEstaAbierto.value).toBe(false)
+
+    expect(result.submenuEstaAbierto.value).toBe(false)
+    result.alternarSubmenu()
+    expect(result.submenuEstaAbierto.value).toBe(true)
+    result.alternarSubmenu()
+    expect(result.submenuEstaAbierto.value).toBe(false)
   })
 })
