dd90243f891e87e39a135d9e856204eaec0f1b96
flkt <flkt.crnpio@gmail.com>
2024-11-29 17:13:37 -0600

    creando componentes de casilla de verificacion y grupo de casillas de verificaion. agregando documentacion de nuevos componentes y actualizando readme y changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,12 @@ de la biblioteca. Por lo tanto no se listan aquí todos los tags existentes.
 Al principio de cada entrada se lista la versión de la biblioteca de sisdai-css con la
 que la versión indicada de sisdai-componentes es compatible y tiene instalada.
 
+## [4.7.0] - 2024-11-29
+
+### Agregado (Added)
+
+- Se agrega el componente SisdaiCasillaVerificacion y SisdaiCasillasVerificacionGrupo para mejorar la accesibilidad en elementos de casillas de verificación
+
 ## [4.6.0] - 2024-11-25
 
 ### Agregado (Added)
@@ -187,6 +193,7 @@ sisdai-css v0.37.3
 
 Primera versión estable
 
+[4.4.0]: https://codigo.conahcyt.mx/sisdai/sisdai-componentes/compare/v4.7.0...v4.6.0
 [4.4.0]: https://codigo.conahcyt.mx/sisdai/sisdai-componentes/compare/v4.6.0...v4.5.0
 [4.4.0]: https://codigo.conahcyt.mx/sisdai/sisdai-componentes/compare/v4.5.0...v4.4.0
 [4.4.0]: https://codigo.conahcyt.mx/sisdai/sisdai-componentes/compare/v4.4.0...v4.3.2
---
+++ src/componentes/casilla-verificacion/SisdaiCasillaVerificacion.vue
@@ -0,0 +1,104 @@
+<!--This file is part of sisdai-componentes.-->
+
+<!--sisdai-componentes is free software: you can redistribute it and/or modify-->
+<!--it under the terms of the GNU Lesser General Public License as published by the-->
+<!--Free Software Foundation, either version 3 of the License, or-->
+<!--(at your option) any later version.-->
+
+<!--sisdai-componentes is distributed in the hope that it will be useful,-->
+<!--but WITHOUT ANY WARRANTY; without even the implied warranty of-->
+<!--MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General-->
+<!--Public License for more details.-->
+
+<!--You should have received a copy of the GNU Lesser General Public License along-->
+<!--with sisdai-componentes. If not, see <https://www.gnu.org/licenses/>.-->
+
+<script setup>
+  import { computed } from 'vue'
+
+  const identificador = idAleatorio()
+
+  function idAleatorio() {
+    return 'casilla-' + Math.random().toString(36).substring(2)
+  }
+
+  defineExpose({
+    identificador,
+  })
+
+  const props = defineProps({
+    titulo: {
+      type: String,
+    },
+    etiqueta: {
+      type: String,
+      required: true
+    },
+    texto_ayuda: {
+      type: String,
+      default: ''
+    },
+    texto_error: {
+      type: String,
+      default: ''
+    },
+    modelValue: {
+      type: [Boolean, Array],
+      required: true,
+    },
+    es_obligatorio: {
+      type: Boolean,
+      default: false
+    },
+    es_ayuda_visible: {
+      type: Boolean,
+      default: false
+    },
+  });
+
+  const emit = defineEmits(['update:modelValue'])
+
+  const modeloCasilla = computed({
+    get() {
+      return props.modelValue;
+    },
+    set(value) {
+      emit('update:modelValue', value)
+    }
+  })
+</script>
+
+<template>
+  <span>
+    <p v-if="titulo" class="titulo-leyenda">
+      {{ titulo }}
+      <span 
+        v-if="es_obligatorio"
+        class="formulario-obligatoriedad" 
+      >
+        (Obligatorio)
+      </span>
+    </p>
+    <input 
+      :id="identificador"
+      :required="es_obligatorio"
+      :aria-required="es_obligatorio"
+      v-model="modeloCasilla"
+      type="checkbox"
+      v-bind="$attrs"
+    />
+    <label :for="identificador">
+      {{ etiqueta }}
+      <span 
+        v-if="es_obligatorio && !titulo"
+        class="formulario-obligatoriedad" 
+      >
+        (Obligatorio)
+      </span>
+    </label>
+    <p aria-live="polite" class="formulario-ayuda" role="status" v-if="es_ayuda_visible && (texto_ayuda || es_obligatorio || texto_error)"> 
+      {{ texto_error }}
+      {{ texto_ayuda }}
+    </p>
+  </span>
+</template>
